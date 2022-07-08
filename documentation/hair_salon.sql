b0b8cadc576177559fcad642f1fbe51b56044fa1
genesisd1005 <sanchezgenesisd@gmail.com>
2022-07-08 11:26:41 -0400

    bd

--- documentation/hair_salon.sql
+++ documentation/hair_salon.sql
@@ -1,272 +1,476 @@
 -- phpMyAdmin SQL Dump
--- version 2.10.3
--- http://www.phpmyadmin.net
--- 
--- Servidor: localhost
--- Tiempo de generación: 07-07-2022 a las 12:31:37
--- Versión del servidor: 5.0.51
--- Versión de PHP: 5.2.6
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2022 at 02:37 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- 
--- Base de datos: `hair_salon`
--- 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hair_salon`
+--
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `assistance`
--- 
+--
+-- Table structure for table `assistance`
+--
 
 CREATE TABLE `assistance` (
-  `id` int(11) NOT NULL auto_increment,
-  `date` datetime NOT NULL,
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
   `entry_time` time NOT NULL,
   `departure_time` time NOT NULL,
-  `id_users` int(11) NOT NULL,
-  `observations` varchar(100) collate utf8_spanish_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `assistance`
--- 
-
+  `id_user` int(11) NOT NULL,
+  `observations` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `clients`
--- 
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
 
 CREATE TABLE `clients` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `last_name` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `phone` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `address` varchar(100) collate utf8_spanish_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `clients`
--- 
-
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `last_name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `phone` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `address` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `payment_methods`
--- 
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `id` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `id_quote` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_client_service`
+--
+
+CREATE TABLE `payment_client_service` (
+  `id` int(11) NOT NULL,
+  `id_service_contract` int(11) NOT NULL,
+  `id_payment_method` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_methods`
+--
 
 CREATE TABLE `payment_methods` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `payment_methods`
--- 
-
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `payment_users_service`
--- 
+--
+-- Table structure for table `payment_users_service`
+--
 
 CREATE TABLE `payment_users_service` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL,
   `id_service_contract` int(11) NOT NULL,
-  `id_payment_methods` int(11) NOT NULL,
+  `id_payment_method` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
   `date` datetime NOT NULL,
-  `observations` varchar(100) collate utf8_spanish_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `payment_users_service`
--- 
-
+  `observations` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `quote`
--- 
+--
+-- Table structure for table `quote`
+--
 
 CREATE TABLE `quote` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `quote`
--- 
-
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `roles`
--- 
+--
+-- Table structure for table `roles`
+--
 
 CREATE TABLE `roles` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `funtions` varchar(100) collate utf8_spanish_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `roles`
--- 
-
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `functions` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `services`
--- 
+--
+-- Table structure for table `services`
+--
 
 CREATE TABLE `services` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(100) collate utf8_spanish_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `date` datetime NOT NULL,
-  `id_type_services` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `services`
--- 
-
+  `id_category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `service_client_payment`
--- 
-
-CREATE TABLE `service_client_payment` (
-  `id` int(11) NOT NULL auto_increment,
-  `id_service_contract` int(11) NOT NULL,
-  `id_payment_methods` int(11) NOT NULL,
-  `id_clients` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `service_client_payment`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- Estructura de tabla para la tabla `service_contract`
--- 
+--
+-- Table structure for table `service_contract`
+--
 
 CREATE TABLE `service_contract` (
-  `id` int(11) NOT NULL auto_increment,
-  `id_users` int(11) NOT NULL,
-  `id_services` int(11) NOT NULL,
-  `id_type_services` int(11) NOT NULL,
-  `id_service_detail` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_service` int(11) NOT NULL,
   `id_quote` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `observations` varchar(100) collate utf8_spanish_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `service_contract`
--- 
-
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `observations` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `service_detail`
--- 
+--
+-- Table structure for table `service_detail`
+--
 
 CREATE TABLE `service_detail` (
-  `id` int(11) NOT NULL auto_increment,
-  `id_services` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `price` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `service_detail`
--- 
-
+  `id` int(11) NOT NULL,
+  `id_service` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `type_services`
--- 
-
-CREATE TABLE `type_services` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `type_services`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- Estructura de tabla para la tabla `users`
--- 
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `name_lastname` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `dni` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `phone` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `address` varchar(100) collate utf8_spanish_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `id_roles` int(11) NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `dni` (`dni`,`phone`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- 
--- Volcar la base de datos para la tabla `users`
--- 
-
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `lastname` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `dni` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `phone` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `address` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `email` text COLLATE utf8_spanish_ci NOT NULL,
+  `user_name` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `id_user_rol` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
--- 
--- Estructura de tabla para la tabla `user_role`
--- 
+--
+-- Table structure for table `user_role`
+--
 
 CREATE TABLE `user_role` (
-  `id` int(11) NOT NULL auto_increment,
-  `id_users` int(11) NOT NULL,
-  `id_roles` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- 
--- Volcar la base de datos para la tabla `user_role`
--- 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `assistance`
+--
+ALTER TABLE `assistance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_client` (`id_client`),
+  ADD KEY `id_quote` (`id_quote`);
+
+--
+-- Indexes for table `payment_client_service`
+--
+ALTER TABLE `payment_client_service`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_service_contract` (`id_service_contract`),
+  ADD KEY `id_payment_method` (`id_payment_method`),
+  ADD KEY `id_client` (`id_client`);
+
+--
+-- Indexes for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment_users_service`
+--
+ALTER TABLE `payment_users_service`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_service_contract` (`id_service_contract`),
+  ADD KEY `id_payment_method` (`id_payment_method`);
+
+--
+-- Indexes for table `quote`
+--
+ALTER TABLE `quote`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_category` (`id_category`);
+
+--
+-- Indexes for table `service_contract`
+--
+ALTER TABLE `service_contract`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_service` (`id_service`),
+  ADD KEY `id_quote` (`id_quote`);
+
+--
+-- Indexes for table `service_detail`
+--
+ALTER TABLE `service_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_service` (`id_service`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_name` (`user_name`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `email` (`email`) USING HASH;
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assistance`
+--
+ALTER TABLE `assistance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_client_service`
+--
+ALTER TABLE `payment_client_service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_users_service`
+--
+ALTER TABLE `payment_users_service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quote`
+--
+ALTER TABLE `quote`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service_contract`
+--
+ALTER TABLE `service_contract`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service_detail`
+--
+ALTER TABLE `service_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assistance`
+--
+ALTER TABLE `assistance`
+  ADD CONSTRAINT `assistance_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `invoices_ibfk_2` FOREIGN KEY (`id_quote`) REFERENCES `quote` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payment_client_service`
+--
+ALTER TABLE `payment_client_service`
+  ADD CONSTRAINT `payment_client_service_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `payment_client_service_ibfk_2` FOREIGN KEY (`id_payment_method`) REFERENCES `payment_methods` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `payment_client_service_ibfk_3` FOREIGN KEY (`id_service_contract`) REFERENCES `service_contract` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payment_users_service`
+--
+ALTER TABLE `payment_users_service`
+  ADD CONSTRAINT `payment_users_service_ibfk_1` FOREIGN KEY (`id_payment_method`) REFERENCES `payment_methods` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `payment_users_service_ibfk_2` FOREIGN KEY (`id_service_contract`) REFERENCES `service_contract` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `services`
+--
+ALTER TABLE `services`
+  ADD CONSTRAINT `services_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `service_contract`
+--
+ALTER TABLE `service_contract`
+  ADD CONSTRAINT `service_contract_ibfk_1` FOREIGN KEY (`id_quote`) REFERENCES `quote` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `service_contract_ibfk_2` FOREIGN KEY (`id_service`) REFERENCES `services` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `service_contract_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `service_detail`
+--
+ALTER TABLE `service_detail`
+  ADD CONSTRAINT `service_detail_ibfk_1` FOREIGN KEY (`id_service`) REFERENCES `services` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
