7e6b0c71df26ef2a7bb587ee80b1ee3f7fa6ae98
daharniel90 <daharniel0890@gmail.com>
2022-07-08 16:55:25 -0400

    form methods payment

--- components/payments/paymentMethods/paymentMethodsRegisterComponent.php
+++ components/payments/paymentMethods/paymentMethodsRegisterComponent.php
@@ -24,45 +24,34 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Registro metodos de pago</h1>
+        <div class="row ">
+          <div class="col-6 offse-3">
+            
           </div>
         </div>
       </div>
     </section>
 
               <!-- form start -->
-              <form id="quickForm">
+              <div class= "row">
+                  <div class="col-6 offset-3"> 
+                  <h1 class="text-center">Registro métodos de pago</h1>
+                   <form id="quickForm">
                 <div class="card-body">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Nombre</label>
                     <input type="text" name="nameU" class="form-control" id="" placeholder="Nombre">
                   </div>
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Apellido</label>
-                    <input type="text" name="LnameU" class="form-control" id="" placeholder="Apellido">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input type="email" name="email" class="form-control" id="" placeholder="Email">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Contraseña</label>
-                    <input type="password" name="password" class="form-control" id="" placeholder="Contraseña">
-                  </div>
-                  <div class="form-group mb-0">
-                    <div class="custom-control custom-checkbox">
-                      <input type="checkbox" name="terms" class="custom-control-input" id="">
-                      <label class="custom-control-label" for="exampleCheck1">Acepto los<a href="#">terminos</a>.</label>
-                    </div>
-                  </div>
-                </div>
+              </div>
                 <!-- /.card-body -->
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Enviar</button>
+                <div class="text-right">
+                  <button type="submit" class="btn btn-primary">Registrar</button>
                 </div>
-              </form>
+              </form> 
+                  </div>
+
+              </div>
+              
             </div>
             <!-- /.card -->
             </div>
