f8ef7ec41423b2be9fe40bf77544b0278e881661
daharniel90 <daharniel0890@gmail.com>
2022-07-14 18:04:03 -0400

    register add

--- components/users/roles/rolRegisterComponent.php
+++ components/users/roles/rolRegisterComponent.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HairOneSalom</title>
+    <title>HairOneSalon</title>
 
      <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -22,57 +22,31 @@
  
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Registro de roles</h1>
-          </div>
-        </div>
-      </div>
-    </section>
+  
 
               <!-- form start -->
-              <form id="quickForm">
-                <div class="card-body">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Nombre</label>
-                    <input type="text" name="nameU" class="form-control" id="" placeholder="Nombre">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Apellido</label>
-                    <input type="text" name="LnameU" class="form-control" id="" placeholder="Apellido">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input type="email" name="email" class="form-control" id="" placeholder="Email">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Contraseña</label>
-                    <input type="password" name="password" class="form-control" id="" placeholder="Contraseña">
-                  </div>
-                  <div class="form-group mb-0">
-                    <div class="custom-control custom-checkbox">
-                      <input type="checkbox" name="terms" class="custom-control-input" id="">
-                      <label class="custom-control-label" for="exampleCheck1">Acepto los <a href="#">terminos</a>.</label>
-                    </div>
-                  </div>
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Enviar</button>
-                </div>
-              </form>
-            </div>
-            <!-- /.card -->
-            </div>
+              <div class= "row">
+                <div class="col-6 offset-3"> 
+                  <h1 class="text-center">Registro de roles</h1>
+                   <form id="quickForm">
+                      <div class="card-body">
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Nombre</label>
+                          <input type="text" name="nameU" class="form-control" id="" placeholder="Nombre">
+                        </div>
+                    
+                        <!-- /.card-body -->
+                        <div class="text-right">
+                          <button type="submit" class="btn btn-primary">Registrar</button>
+                        </div>
+                      </div>
+                  </form>
          
-          <!--/.col (right) -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
+                    <!--/.col (right) -->
+              </div>
+        
+            </div><!-- /.row -->
+  
     <!-- /.content -->
   </div>
   <?php include("./../../../components/commons/footerComponent.php")?>
