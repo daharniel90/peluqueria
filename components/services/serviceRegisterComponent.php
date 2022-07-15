669b5a5d47aa0e015fe28cb8e28ff47cc710e7d2
genesisd1005 <sanchezgenesisd@gmail.com>
2022-07-14 20:35:22 -0400

    select service

--- components/services/serviceRegisterComponent.php
+++ components/services/serviceRegisterComponent.php
@@ -63,6 +63,15 @@
                     <input type="text" name="name" class="form-control" id="" placeholder="Nombre">
                     
                 </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1" class="form-label">categoria:</label>
+                    <select class="form-select" aria-label=".form-select-sm example" name="nacionalidad">
+                        <option value="">-</option>
+                        <option value=""></option>
+                        <option value=""></option>
+                    </select>
+                    
+                </div>
                 </div>
                 <!-- /.card-body -->
                 <div class="card-footer">
