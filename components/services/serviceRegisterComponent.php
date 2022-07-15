546dca4425e46af5d46fa6be117bd930437fb65e
genesisd1005 <sanchezgenesisd@gmail.com>
2022-07-14 20:40:46 -0400

    select service2

--- components/services/serviceRegisterComponent.php
+++ components/services/serviceRegisterComponent.php
@@ -58,20 +58,18 @@
               <!-- form start -->
               <form id="quickForm" method="post" action="?">
                 <div class="card-body">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Nombre</label>
-                    <input type="text" name="name" class="form-control" id="" placeholder="Nombre">
-                    
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1" class="form-label">categoria:</label>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1" class="form-label">categoria:</label>
                     <select class="form-select" aria-label=".form-select-sm example" name="nacionalidad">
                         <option value="">-</option>
                         <option value=""></option>
                         <option value=""></option>
-                    </select>
-                    
-                </div>
+                    </select> 
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Nombre</label>
+                    <input type="text" name="name" class="form-control" id="" placeholder="Nombre">
+                  </div>
                 </div>
                 <!-- /.card-body -->
                 <div class="card-footer">
